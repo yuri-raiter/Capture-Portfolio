e799e75c5ec6f7aa0036a8087cdf07670fda1069
yuri-raiter <yurinraiter@gmail.com>
2021-01-25 18:49:01 -0300

    styled about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+// Global Style
+import GlobalStyle from './components/GlobalStyle'
+// Pages
 import AboutUs from './pages/AboutUs'
 
 const App = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
