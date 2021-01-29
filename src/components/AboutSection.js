b2db53e4c171dd8f5f8a9798a6f62cbaf85441d4
yuri-raiter <yurinraiter@gmail.com>
2021-01-28 22:32:06 -0300

    About Section animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,6 +4,7 @@ import home1 from '../img/home1.png'
 
 import { Wrapper, Description, Image, Hide } from '../styles'
 import { motion } from 'framer-motion'
+import { titleAnimation, fade, photoAnimation } from '../animation'
 
 
 const AboutSection = () => {
@@ -13,20 +14,20 @@ const AboutSection = () => {
             <Description>
                 <motion.div>
                     <Hide>
-                        <motion.h2>We work to make</motion.h2>
+                        <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>your <span>dreams</span> come</motion.h2>
+                        <motion.h2 variants={titleAnimation}>your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>true.</motion.h2>
+                        <motion.h2 variants={titleAnimation}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</p>
-                <button>Contact us</button>
+                <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</motion.p>
+                <motion.button variants={fade}>Contact us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with a camera"/>
+                <motion.img variants={photoAnimation} src={home1} alt="guy with a camera"/>
             </Image>
         </Wrapper>
     )
